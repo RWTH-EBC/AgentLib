eb8026fba0b275b0fde737b703d9371e9cccccbe
felix.stegemerten <felix.stegemerten@eonerc.rwth-aachen.de>
2024-02-23 11:42:29 +0100

    Add start time to step function

--- agentlib/modules/simulator.py
+++ agentlib/modules/simulator.py
@@ -410,7 +410,8 @@ class Simulator(BaseModule):
         _end_time_simulation = self.env.time + self.config.t_sample
         # Simulate
         self.model.do_step(
-            t_start=(self.env.now + self.env.offset), t_sample=self.config.t_sample
+            t_start=(self.env.now + self.env.offset + self.config.t_start),
+            t_sample=self.config.t_sample
         )
         # Update the results and outputs
         self._update_results(_end_time_simulation)
