d43ca6ace3b572efddaf8fd0dffd542e8579ecab
ses <steffen.eser@eonerc.rwth-aachen.de>
2024-07-03 16:19:00 +0200

    current status: can save configs to clipboard

--- agentlib/utils/available_modules/available_modules.py
+++ agentlib/utils/available_modules/available_modules.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, jsonify
+import json
+from typing import get_origin
+
+from flask import Flask, render_template
 from pydantic import BaseModel
 
 from agentlib.modules import get_all_module_types
@@ -19,40 +22,50 @@ def get_config_info(config_class):
     return fields
 
 
-def get_default_json(config_class):
-    default_values = {}
+def get_default_value(field):
+    if field.default is not None:
+        return field.default
+    if get_origin(field.annotation) is list:
+        return []
+    if get_origin(field.annotation) is dict:
+        return {}
+    return None
+
+
+def get_model_config(config_class):
+    config = {}
     for name, field in config_class.model_fields.items():
-        default_values[name] = field.default if field.default is not None else None
-    return jsonify(default_values).get_data(as_text=True)
+        config[name] = get_default_value(field)
+    return json.dumps(config, indent=2, default=str)
 
 
 @app.route("/")
 def index():
     modules = get_all_module_types(plugins=[])
     module_configs = {}
-    default_jsons = {}
+    model_configs = {}
     for key, module in modules.items():
         try:
             class_ = module.import_class()
             config_type = class_.get_config_type()
             if issubclass(config_type, BaseModel):
                 module_configs[key] = get_config_info(config_type)
-                default_jsons[key] = get_default_json(config_type)
+                model_configs[key] = get_model_config(config_type)
             else:
                 module_configs[key] = [
                     {"error": "Config class is not a subclass of pydantic.BaseModel"}
                 ]
-                default_jsons[key] = "{}"
+                model_configs[key] = "{}"
         except Exception as e:
             module_configs[key] = [{"error": str(e)}]
-            default_jsons[key] = "{}"
+            model_configs[key] = "{}"
 
     sorted_modules = sorted(modules.items(), key=lambda x: x[1].import_path)
     return render_template(
         "index.html",
         modules=sorted_modules,
         configs=module_configs,
-        default_jsons=default_jsons,
+        model_configs=model_configs,
     )
 
 
