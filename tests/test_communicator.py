875a965271ecf12d2f61c4f34b409cd20c8a2d85
ses <steffen.eser@eonerc.rwth-aachen.de>
2024-11-26 13:54:25 +0100

    fix pd series in communicator

--- tests/test_communicator.py
+++ tests/test_communicator.py
@@ -1,5 +1,7 @@
 import unittest
 
+import pandas as pd
+
 from agentlib import Agent, Environment, AgentVariable
 from agentlib.modules.communicator.local import LocalClient
 
@@ -28,7 +30,6 @@ class TestCommunicator(unittest.TestCase):
         self.agent_send = Agent(config={"id": "Send", "modules": []}, env=Environment())
         self.agent_rec = Agent(config={"id": "Rec", "modules": []}, env=Environment())
 
-    @unittest.skip
     def test_roundtrip_variable(self):
         variable = AgentVariable(**default_data)
         comm = LocalClient(config=self.test_config, agent=self.agent_send)
@@ -40,6 +41,16 @@ class TestCommunicator(unittest.TestCase):
             self.assertEqual(
                 variable.__getattribute__(field), variable2.__getattribute__(field)
             )
+
+    def test_pd_series(self):
+        """Tests whether pandas series are sent correctly"""
+        data = {**default_data, "value": pd.Series({0: 1, 10: 2}), "type": "pd.Series"}
+        variable = AgentVariable(**data)
+        comm = LocalClient(config=self.test_config, agent=self.agent_send)
+        payload = comm.short_dict(variable)
+        var_json = comm.to_json(payload)
+        variable2 = AgentVariable.from_json(var_json)
+        pd.testing.assert_series_equal(variable.value, variable2.value)
 
 
 if __name__ == "__main__":
